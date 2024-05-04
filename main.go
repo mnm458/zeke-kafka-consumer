8e6ee3bc559e782217b39b9d37a78b895521469f
Mohak <mohaknainmalhotra@gmail.com>
2024-05-04 21:12:33 +1000

    main refactoring

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	invoice "github.com/mnm458/zeke-kafka-consumer/pkg/invoice"
 	app "github.com/mnm458/zeke-kafka-consumer/pkg/server"
 
-	"encoding/binary"
 	"log"
 	"math/big"
 
@@ -12,72 +11,31 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
 )
 
-const myABIJSON = `[
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "_onramper",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "_token",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "_amount",
-                "type": "uint256"
-            },
-            {
-                "internalType": "int256",
-                "name": "_minFiatRate",
-                "type": "int256"
-            },
-            {
-                "internalType": "uint64",
-                "name": "_dstChainId",
-                "type": "uint64"
-            }
-        ],
-        "name": "addOrder",
-        "outputs": [
-            {
-                "internalType": "bytes32",
-                "name": "",
-                "type": "bytes32"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "state",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-]`
+type ParsedTx struct {
+	onRamper string
+	amount   *big.Int
+	intentID string
+}
 
+//const myABIJSON = unmarshal
+
+/*
+ReadConfig()
+Reads the client configuration from client.properties
+Returns it as a key-value map
+*/
 func ReadConfig() kafka.ConfigMap {
-	// reads the client configuration from client.properties
-	// and returns it as a key-value map
+	//
 	m := make(map[string]kafka.ConfigValue)
 	file, err := os.Open("client.properties")
 	if err != nil {
@@ -105,27 +63,16 @@ func ReadConfig() kafka.ConfigMap {
 	return m
 }
 
-func main() {
-
-	const url = "https://rpc.ankr.com/base_sepolia/5b3e494193a39bb51d06eb3cb4735c305870850adcd0987f03bd6f88ea308737"
-	rpcClient, err := ethclient.Dial(url)
-
-	if err != nil {
-		panic(err)
-	}
-	defer rpcClient.Close()
-	txHash := common.HexToHash("0xdc9b7a34ca6699872049836692832a1dd54acfe2bef6178d636627d626ff55b3")
-	//functionSignature := "addOrder(address, address, uint256, int256 , uint64) bytes32"
-	//selector := crypto.Keccak256([]byte(functionSignature))[:4]
-
-	tx, isPending, err := rpcClient.TransactionByHash(context.Background(), txHash)
-
-	if isPending {
-		log.Fatal("Pending transaction cannot be processed")
-	}
-
-	if err != nil {
-		log.Fatal(err)
+/*
+GethParseTx()
+Reads the client configuration from client.properties
+Returns it as a key-value map
+*/
+func GethParseTx(client *ethclient.Client, txString string) ParsedTx {
+	txHash := common.HexToHash(txString)
+	tx, isPending, txErr := client.TransactionByHash(context.Background(), txHash)
+	if txErr != nil {
+		log.Fatal("Transaction retrival failed: ", txErr)
 	}
 	inputData := tx.Data()
 
@@ -133,49 +80,58 @@ func main() {
 		log.Fatal("Invalid input data length")
 	}
 
-	// Decode the input data based on the parameter types
-	param1 := common.BytesToAddress(inputData[4:36])
-	param2 := common.BytesToAddress(inputData[36:68])
-	param3 := new(big.Int).SetBytes(inputData[68:100])
-	param4 := new(big.Int).SetBytes(inputData[100:132])
-	param5 := binary.BigEndian.Uint64(inputData[132:140])
-
-	fmt.Printf("Onramper addreess: %s\n", param1.Hex())
-	fmt.Printf("Stablecoin address: %s\n", param2.Hex())
-	fmt.Printf("Amount: %s\n", param3.String())
-	fmt.Printf("MinFiatRate: %s\n", param4.String())
-	fmt.Printf("Chain ID: %d\n", param5)
-
-	receipt, err := rpcClient.TransactionReceipt(context.Background(), txHash)
-	if err != nil {
-		log.Fatal(err)
+	if isPending {
+		log.Fatal("Pending transaction cannot be processed")
 	}
+	onRamper := common.BytesToAddress(inputData[4:36]).Hex()
+	fmt.Printf("Onramper address: %s\n", onRamper)
+	amount := new(big.Int).SetBytes(inputData[68:100])
+	fmt.Printf("Amount: %s\n", amount.String())
 
+	parsedTx := ParsedTx{onRamper: onRamper, amount: amount, intentID: GethRetrieveLog(client, txHash)}
+	return parsedTx
+}
+
+func GethRetrieveLog(client *ethclient.Client, txHash common.Hash) string {
+	receipt, receiptErr := client.TransactionReceipt(context.Background(), txHash)
+	var intentID string
+	if receiptErr != nil {
+		log.Fatal("Receipt retrieval failed: ", receiptErr)
+	}
 	// Check if the transaction was successful
 	if receipt.Status == types.ReceiptStatusSuccessful {
-		// Assuming the function returns a single value
 		if len(receipt.Logs) > 0 {
 			// Get the first log entry
 			log := receipt.Logs[0]
-
 			// Extract the return value from the log data
-			returnValue := common.BytesToHash(log.Data)
-
-			fmt.Printf("Return Value: %s\n", string(returnValue.Hex()))
-
-			int64Val := param3.Int64()
-			intVal := int(int64Val)
-			invoice_id, _ := invoice.CreatePayPalInvoice(intVal) //send the txAmount to create PayPal invoice
-			fmt.Println(invoice_id)
+			intentID := common.BytesToHash(log.Data).Hex()
+			fmt.Printf("Intent ID: %v\n", intentID)
 		} else {
-			fmt.Println("No logs found in the transaction receipt")
+			fmt.Println("No intent ID found in the transaction receipt")
 		}
 	} else {
-		fmt.Println("Transaction failed")
+		log.Fatal("Transaction parsing failed")
+	}
+	return intentID
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
 	}
 
-	topic := "orders_topic"
+	apiKey := os.Getenv("ANKR_API_KEY")
+	url := fmt.Sprintf("https://rpc.ankr.com/base_sepolia/%s", apiKey)
+	rpcClient, rpcErr := ethclient.Dial(url)
 
+	if rpcErr != nil {
+		panic(rpcErr)
+	}
+	defer rpcClient.Close()
+
+	//Kafka config
+	topic := "orders_topic"
 	conf := ReadConfig()
 
 	// sets the consumer group ID and offset
@@ -200,6 +156,14 @@ func main() {
 			// TODO: persist key pair - invoice_id, order_id
 			fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
 				*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+			parsedTx := GethParseTx(rpcClient, string(ev.Value))
+			intentIdInt, IntErr := strconv.Atoi(parsedTx.intentID)
+			if IntErr != nil {
+				log.Fatal("Corrupted intent ID")
+			}
+			invoice_id, _ := invoice.CreatePayPalInvoice(intentIdInt)
+			//send the txAmount to create PayPal invoice
+			fmt.Println(invoice_id)
 		case kafka.Error:
 			fmt.Fprintf(os.Stderr, "%% Error: %v\n", ev)
 			run = false
